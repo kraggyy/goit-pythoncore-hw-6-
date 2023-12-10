d6a435a3898c5e85d61708776f30db00a3530833
daniel <den.tymoshenko777@gmail.com>
2023-12-10 15:55:44 +0200

    update

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -1,25 +1,24 @@
-import sys
 from setuptools import setup, find_namespace_packages
-from clean_folder.clean import organize_files
 
-def main():
-    if len(sys.argv) != 2:
-        print("Використання: python sort.py")
-    else:
-        directory = sys.argv[1]
-        known_extensions, unknown_extensions = organize_files(directory)
-
-if __name__ == "__main__":
-    main()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='clean_folder',
     version='1.0.0',
     description='Code which cleans and sorts your directories',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/kraggyy/goit-pythoncore-hw-7',
     author='Daniil Tymoshenko',
     author_email='dan.tym@icloud.com',
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
     packages=find_namespace_packages(),
     install_requires=['markdown'],
+    python_requires=">=3.8",
     entry_points={'console_scripts': ["clean-folder=clean_folder.clean:main"]}
 )
